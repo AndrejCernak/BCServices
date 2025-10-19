f4dbc1298fc3edb907d8025300a4e36f440cd3db
AndrejCernak <165067032+AndrejCernak@users.noreply.github.com>
2025-10-19 22:02:08 +0200

    Remove copyright notice and clean up imports

--- friday_app/bcservices/doctype/friday_user/friday_user.py
+++ friday_app/bcservices/doctype/friday_user/friday_user.py
@@ -1,9 +1,5 @@
-# Copyright (c) 2025, andrej and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class FridayUser(Document):
-	pass
+    pass
